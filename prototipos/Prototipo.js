6a826e97d2bfbe9c38b0314deba1551972c2814d
Alan Sergio Guth <alan_guth@hotmail.com>
2020-09-15 23:27:30 -0300

    adicionado funcionalidade de preenchimento de mes e envio ao js

--- prototipos/Prototipo.js
+++ prototipos/Prototipo.js
@@ -2,10 +2,14 @@ class ano {
      descricao
      listaMes = []
 }
+    var arraymes= [];
+arrayDeCheckboxTrue = function(mes){
+   arraymes.push(mes); 
+}
 
-insereAno = function(valor, listames){  
+insereAno = function(valor){  
    var ano1 = new ano()
    ano1.descricao = valor;
-   ano1.listaMes = listames;
+   ano1.listaMes = arraymes;
 alert('Cadastro realizado, Ano: '+ano1.descricao + ', Meses monitorados: ' + ano1.listaMes)
 }
